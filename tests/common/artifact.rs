27bb809a691b1a788517beb91ffd74e4cb79fc2c
Confucian <confucian.e@gmail.com>
2024-10-31 15:04:22 +0800

    test: pass distribute test

--- tests/common/artifact.rs
+++ tests/common/artifact.rs
@@ -1,45 +1,25 @@
-use alloy::json_abi::JsonAbi;
+use alloy::{hex, json_abi::JsonAbi};
 use eyre::Result;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::{env, fs};
 
-pub fn get_distributor_artifact() -> Result<(JsonAbi, String)> {
+pub fn get_distributor_artifact() -> Result<(JsonAbi, Vec<u8>)> {
     let current_dir = env::current_dir()?;
     let path = current_dir.join("contracts/out/Distributor.sol/Distributor.json");
 
     let file = fs::read_to_string(path)?;
     let content: Artifact = serde_json::from_str(&file)?;
 
-    let abi = serde_json::to_value(content.abi)?;
-    let abi: JsonAbi = serde_json::from_value(abi)?;
-
-    let bytecode = content.bytecode.object;
+    let (abi, bytecode) = (content.abi, content.bytecode.object);
+    let bytecode = hex::decode(&bytecode)?;
 
     Ok((abi, bytecode))
 }
 
 #[derive(Debug, Deserialize)]
 struct Artifact {
-    abi: Vec<AbiComponent>,
+    abi: JsonAbi,
     bytecode: Bytecode,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct AbiComponent {
-    #[serde(rename = "type")]
-    abi_type: String,
-    name: Option<String>,
-    inputs: Option<Vec<AbiInput>>,
-    outputs: Option<Vec<AbiInput>>,
-    state_mutability: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct AbiInput {
-    #[serde(rename = "type")]
-    abi_type: String,
-    name: String,
-    internal_type: Option<String>,
 }
 
 #[derive(Debug, Deserialize)]
